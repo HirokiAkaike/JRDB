e6825bdd94b41ad8c9e34e1403d598b0a5822761
赤池 弘樹 <akaike@lightcafe.co.jp>
2017-12-31 17:22:47 +0900

    マイグレーションファイルを追加

--- db/migrate/20171230084858_create_past_race_results.rb
+++ db/migrate/20171230084858_create_past_race_results.rb
@@ -1,4 +1,4 @@
-class CreateDisassemblePastRaceResults < ActiveRecord::Migration[5.1]
+class CreatePastRaceResults < ActiveRecord::Migration[5.1]
   def change
     create_table :disassemble_past_race_results do |t|
       #レースキー
